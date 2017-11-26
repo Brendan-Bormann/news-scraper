320dca696939c64ffe7e6351ab76db50b3814148
Brendan Bormann <brendan.bormann@gmail.com>
2017-11-25 20:07:38 -0700

    added a lot of functionality and UI

--- datahandler.js
+++ datahandler.js
@@ -1,15 +1,23 @@
 const cheerio = require('cheerio');
 const request = require('request');
 const mongojs = require('mongojs');
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost/test', { useMongoClient: true });
+// mongoose.Promise = global.Promise;
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+// });
 
 // mongodb
-var db = mongojs('newsScrape', ['articles']);
-db.articles.remove({});
+var db = mongojs('newsScrape', ['articles', 'comments']);
+var ObjectId = mongojs.ObjectId;
 
 var dataHandler = {
 
 	update: function(cb) {
-		request('https://www.theguardian.com/world', function (error, response, html) {
+		request('https://www.theguardian.com/us/technology', function (error, response, html) {
 			if (!error && response.statusCode == 200) {
 				console.log('Request successful! Updating database...');
 
@@ -24,23 +32,58 @@ var dataHandler = {
 					var link      = $(element).children('div.fc-item').children('div.fc-item__container').children('div.fc-item__content').children('div.fc-item__header').children('h2.fc-item__title').children('a.fc-item__link').attr('href');
 					var image     = $(element).children('div.fc-item').children('div.fc-item__container').children('div.fc-item__media-wrapper').children('div.fc-item__image-container').children('picture').children('source').attr('srcset');
 
-					if (headline || link || image) {
+					if (headline && link && image) {
+
 						counter2++;
+						image = image.split(' ');
+
 						var post = {
 							headline: headline,
 							link: link,
-							image: image,
+							image: image[0],
 						};
-						db.articles.insert(post);
+						db.articles.update(post, post, {upsert:true});
 					}
 				});
+				cb(true);
 			} else {
 				console.log("Error : " + error);
 			}
 			console.log("Finished data request!");
-			console.log(`Added [${counter2}] entries out of [${counter1}] that were scanned.`);
+			console.log(`Found [${counter2}] valid articles out of [${counter1}] possible that were found.`);
 		});
-		cb(true);
+	},
+
+	retrieveAll: function(cb) {
+		db.articles.find(function (err, docs) {
+			cb(docs);
+		});
+	},
+
+	retrieveOne: function(id, cb) {
+		console.log('ObjectId("' + id + '")');
+		db.articles.findOne({"_id": ObjectId(id)}, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
+		// cb(db.articles.find('ObjectId("' + id + '")'));
+	},
+
+	postComment: function(id, commentName, comment) {
+		var post = {
+			article_id: id,
+			name: commentName,
+			text: comment
+		};
+		
+		db.comments.update(post, post, {upsert:true});
+	},
+
+	findComments: function(id, cb) {
+		db.collection('comments').find({"article_id": ObjectId(id)}, function(err, result) {
+			if (err) throw err;
+			cb(result);
+		});
 	}
 };
 
--- routes/router.js
+++ routes/router.js
@@ -1,31 +1,75 @@
 const express     = require('express');
 const dataHandler = require('../datahandler.js');
 
+
 // express
 var router = express.Router();
 
 // data route
-router.get('/data', function(req, res) {
+router.get('/', function(req, res) {
 
-	var success;
+	// On home load, get new articles and load them into main page
 
+	console.log('Requesting updated data.');
 	dataHandler.update(function(response) {
 		if (response === true) {
-			success = true;
+			displayHome();
 		}
 	});
 
-	res.render('home', {
-		success: success,
-		helpers: {
-			foo: function() {
-				return 'foo.';
-			},
-			bar: function() {
-				return 'bar';
+	var displayData = {
+		data: [],
+		row: [],
+	};
+
+	function displayHome() {
+		dataHandler.retrieveAll(function(articles) {
+			for (i=0; i<articles.length; i++) {
+				displayData.data.push(articles[i]);
+				if ((i+1)% 3 === 0) {
+					displayData.data[i].row = true;
+				}
 			}
-		}
+			if (displayData.data.length > 3) {console.log('Data has returned successfully.');}
+			res.render('home', displayData);
+		});
+	}
+});
+
+router.post("/data/post/:id", function(req, res) {
+	var id = req.params.id;
+	console.log("-------------\n\n\n");
+	console.log("Recieved post request from " + req.body.name + " for article with id of " + id + ".");
+	console.log("\n\n\n-------------");
+	dataHandler.postComment(id, req.body.name, req.body.comment);
+});
+
+router.get("/data/comments/:id", function(req, res) {
+	var id = req.params.id;
+	console.log('Requesting comments page...');
+	console.log(`Id is set to: ${id}`);
+
+	var content = {};
+
+	dataHandler.retrieveOne(id, (response) => {
+		console.log("Content retrieved:");
+		console.log(JSON.stringify(response, null, 2));
+		content.article = response;
+
+		dataHandler.findComments(id, (response) => {
+			console.log(response);
+			content.comments = response;
+			displayComments();
+		});
 	});
+
+	function displayComments() {
+		console.log("Content:");
+		console.log(JSON.stringify(content, null, 2));
+
+		res.render('comment', content);
+	}
+
 });
 
 module.exports = router;
